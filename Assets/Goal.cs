dff9645354958161d6d7e5473bd9f2267ccca496
Paweł Otlewski <potlewski1@edu.cdv.pl>
2019-06-08 17:39:25 +0200

    initial commit

---
+++ Assets/Goal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space) && win.activeSelf)
+            SceneManager.LoadScene(0);
+        if (Input.GetKeyDown(KeyCode.Escape))
+            SceneManager.LoadScene(0);
+    }
+
+    public GameObject win;
+    public GameObject music;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        win.SetActive(true);
+        music.SetActive(false);
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3(PlayerPrefs.GetFloat("x", 0), PlayerPrefs.GetFloat("y", 0), 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed,
+            Input.GetAxis("Vertical") * Time.deltaTime * speed, 0);
+	}
+
+    public GameObject win;
+
+    private void OnApplicationQuit()
+    {
+        if (win.activeSelf) return;
+        PlayerPrefs.SetFloat("x", transform.position.x);
+        PlayerPrefs.SetFloat("y", transform.position.y);
+    }
+}
